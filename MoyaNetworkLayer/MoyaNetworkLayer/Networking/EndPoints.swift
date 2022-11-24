6653e97fee7f96b2415f89bedee4ec7aecf13867
eslamali <eslam.ali4233@gmail.com>
2022-11-24 08:21:27 +0200

    configure Moya networkLayer with promiskit

---
+++ MoyaNetworkLayer/MoyaNetworkLayer/Networking/EndPoints.swift
@@ -0,0 +1,16 @@
+//
+//  EndPoints.swift
+//  BGApp
+//
+//  Created by BasemElgendy on 10/08/2021.
+//
+
+import Foundation
+
+enum EndPoints: String {
+    
+    case countries = "app/countries"
+  case home = "home"
+   
+   
+}
